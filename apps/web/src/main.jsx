842eb69c57c640a358d9b058da256988b45cd8cc
Dimagnic <96718200+Dimagnic@users.noreply.github.com>
2026-06-08 01:27:42 -0600

    fix: modo oscuro solo en plataforma, landing siempre claro

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
-import { ThemeProvider } from 'next-themes';
 import App from '@/App';
 import '@/index.css';
 
-// Desregistrar service workers viejos que causan errores 503
+// Desregistrar service workers viejos
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.getRegistrations().then(registrations => {
     registrations.forEach(reg => reg.unregister());
@@ -14,8 +13,6 @@ if ('serviceWorker' in navigator) {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <HelmetProvider>
-    <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false} storageKey="adn-theme">
-      <App />
-    </ThemeProvider>
+    <App />
   </HelmetProvider>
 );
